0ba39aeeddb5301f71e31e4f3039e07f0b5aa161
marc <marc@LEEPE-MARC>
2021-09-10 22:17:06 +0800

    v09/10/2021

--- AxieArena/wwwroot/js/site.js
+++ AxieArena/wwwroot/js/site.js
@@ -2,20 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-(function ($) {
+$('.plus, .minus').on('click', function (e) {
+    const isNegative = $(e.target).closest('.minus').is('.minus');
+    console.log(isNegative);
+    var isGreater = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.remaining').innerText;
+    console.log(isGreater);
+    const input = $(e.target).closest('.qty').find('input');
+    console.log(input);
+    if (input.is('input')) {
+        //input[0][isNegative ? 'stepDown' : 'stepUp']()
+        if (isNegative) {
+            if (input[0].value > 0)
+                input[0].value--;
+        }
+        else {
+            if (input[0].value < isGreater) {
+                input[0].value++;
+            }
+        }
+    }
+})
 
-	"use strict";
-
-	$('nav .dropdown').hover(function () {
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function () {
-		var $this = $(this);
-		$this.removeClass('show');
-		$this.find('> a').attr('aria-expanded', false);
-		$this.find('.dropdown-menu').removeClass('show');
-	});
-
-})(jQuery);
+//$(document).ready(function () {
+//    $('.count').prop('disabled', true);
+//    $(document).on('click', '.plus', function () {
+//        $('.count').val(parseInt($('.count').val()) + 1);
+//    });
+//    $(document).on('click', '.minus', function () {
+//        $('.count').val(parseInt($('.count').val()) - 1);
+//        if ($('.count').val() == 0) {
+//            $('.count').val(1);
+//        }
+//    });
+//});
